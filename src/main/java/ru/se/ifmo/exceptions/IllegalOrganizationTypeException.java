45dbcc51898d6b761d960a1d8a4c7c6dbe05f9c3
GloRRian55 <GloRRian55@yandex.ru>
2024-05-31 13:13:28 +0300

    update

--- src/main/java/ru/se/ifmo/exceptions/IllegalOrganizationTypeException.java
+++
@@ -1,7 +0,0 @@
-package ru.se.ifmo.exceptions;
-
-public class IllegalOrganizationTypeException extends Exception{
-    public IllegalOrganizationTypeException(String message) {
-        super(message);
-    }
-}
